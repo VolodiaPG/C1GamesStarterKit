c00d88c9ff3f39cbd744b54ea38ddf95dbd54624
volodiapg <volodia.parolguarino@gmail.com>
2020-04-28 12:16:06 +0200

    Trying to make the gym environement work with the external engine.jar

--- rl-algo/rl/terminal-gym/envs/terminal_env_v0/terminal_env.py
+++ rl-algo/rl/terminal-gym/envs/terminal_env_v0/terminal_env.py
@@ -1,10 +1,69 @@
+import logging
 import gym
+
+LOG_FMT = logging.Formatter('%(levelname)s '
+                            '[%(filename)s:%(lineno)d] %(message)s',
+                            '%Y-%m-%d %H:%M:%S')
+
+def run_single_game(process_command):
+    print("Start run a match")
+    p = subprocess.Popen(
+        process_command,
+        shell=True,
+        stdout=sys.stdout,
+        stderr=sys.stderr
+        )
+    # daemon necessary so game shuts down if this script is shut down by user
+    p.daemon = 1
+    p.wait()
+    print("Finished running match")
+
+
+# Get location of this run file
+file_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.join(file_dir, os.pardir)
+parent_dir = os.path.abspath(parent_dir)
+def run():
 
 class TerminalEnv(gym.env):
 
     def __init__(self):
-        print(‘Environment initialized’)
-    def step(self):
-        print(‘Step successful!’)
+        self.seed()
+        logging.info(‘Environment initialized’)
+    def step(self, action):
+        logging.info(‘Step successful!’)
     def reset(self):
-        print(‘Environment reset’)
+        logging.info(‘Environment reset’)
+    
+    def set_log_level_by(verbosity):
+    """Set log level by verbosity level.
+    verbosity vs log level:
+        0 -> logging.ERROR
+        1 -> logging.WARNING
+        2 -> logging.INFO
+        3 -> logging.DEBUG
+    Args:
+        verbosity (int): Verbosity level given by CLI option.
+    Returns:
+        (int): Matching log level.
+    """
+    if verbosity == 0:
+        level = 40
+    elif verbosity == 1:
+        level = 30
+    elif verbosity == 2:
+        level = 20
+    elif verbosity >= 3:
+        level = 10
+
+    logger = logging.getLogger()
+    logger.setLevel(level)
+    if len(logger.handlers):
+        handler = logger.handlers[0]
+    else:
+        handler = logging.StreamHandler()
+        logger.addHandler(handler)
+
+    handler.setLevel(level)
+    handler.setFormatter(LOG_FMT)
+    return level
